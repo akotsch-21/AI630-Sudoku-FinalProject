08a146cc163b488cf7cafd72094fceda632ee2b6
Jean Luis Urena <jlurena@teladochealth.com>
2026-03-17 23:22:08 -0400

    Use API to fetch single row instead of entire set

--- code/models/board.py
+++ code/models/board.py
@@ -1,24 +1,31 @@
 from models.cage import Cage
 from models.cell import Cell
 import random
-import pandas as pd
+import requests
 
 
 class Board:
     MAX_LEN = 81
-    PARQUET_PATH = "hf://datasets/jackcai1206/killer-sudoku-puzzles/data/train-00000-of-00001.parquet"
+    MAX_PUZZLE_ROWS = 960 # As per HuggingFace dataset info.
+    PUZZLE_URI = "https://datasets-server.huggingface.co/rows?dataset=jackcai1206%2Fkiller-sudoku-puzzles&config=default&split=train"
 
     @classmethod
     def load_random_puzzle(cls) -> "Board":
-        # Load the puzzle
-        df_sudoku = pd.read_parquet(cls.PARQUET_PATH)
-        puzzle_strings = df_sudoku["puzzle_string"].to_list()
-        difficulties = df_sudoku["difficulty"].to_list()
+        """
+        Load a random puzzle from the parquet file and construct the sudoku Board object.
 
-        # Select at random
-        random_row = random.randint(0, len(puzzle_strings) - 1)
-        puzzle_string = puzzle_strings[random_row]
-        difficulty = difficulties[random_row]
+        Returns:
+            Board: A Board object representing the loaded puzzle.
+        """
+
+        # Download a random row from hugging face.
+        random_row = random.randint(0, cls.MAX_PUZZLE_ROWS - 1)
+        response = requests.get(url = cls.PUZZLE_URI + f"&offset={random_row}&length=1", timeout=10)
+        data = response.json()
+        row = data["rows"][0]["row"]
+        puzzle_string = row["puzzle_string"]
+        difficulty = row["difficulty"] # We're always just pulling one row, so take first.
+
         layout, sums_part = puzzle_string.split("\n", 1)
 
         if len(layout) != cls.MAX_LEN:
