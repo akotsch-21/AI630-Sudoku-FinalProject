882e53c593f9b94023487a84a238a337e743da75
Aidan <awk6957@g.rit.edu>
2026-04-16 17:07:58 -0400

    Backtracking

--- code/main.py
+++ code/main.py
@@ -1,22 +1,100 @@
 if __name__ == "__main__":
     import time
-
+    import copy
     from rich.live import Live
     from models.board import Board
+    from models.backtracking import backtracking
+    from models.backtracking import backtrackModified
 
+    from models.backtracking import backtrackingWithForwardChecking
+   
     board = Board.load_random_puzzle(difficulty=4)
-    print(f"Before revising {board}")
+    boardmod = copy.deepcopy(board)
+    Fboard = copy.deepcopy(board)
+    
+    #print(f"Before revising {board}")
     # print(board)
-    board.revise_arcs()
-    print(f"Domains after revising {board}")
+
+    
+    #print(f"Domains after revising {board}")
     # print(board)
 
 
-    # with Live(board, refresh_per_second=20) as live:
+    #board = backtracking(board, 0, 0)
+    #print(f"After backtracking {board}")
 
-    #     while not board.solved:
-    #         board.revise_arcs()
-    #         live.update(board, refresh=True)
-    #         time.sleep(0.03)
 
-    #     live.update(board, refresh=True)
+
+
+with Live(board, refresh_per_second=20) as live:
+    start = time.time()
+
+    #print(cell.row, cell.col, cell.domains.intersection(cell.cage.getDomain()))
+
+    correct, backtracks = backtracking(board, 0,0,0) # normal backtracking
+    
+    end = time.time()
+
+    #live.update(board, refresh=True)
+    print(f"Backtracking: Time taken: {end - start:.2f} seconds, Backtracking calls saved: {board.backtrack_calls-backtracks}")
+
+with Live(boardmod, refresh_per_second=20) as live:
+    start = time.time()
+
+    #print(cell.row, cell.col, cell.domains.intersection(cell.cage.getDomain()))
+
+    correct, backtracks = backtrackModified(boardmod, boardmod.select_unassigned_cell(),0) # normal backtracking
+    
+    end = time.time()
+
+    #live.update(board, refresh=True)
+    print(f"Backtracking Modified: Time taken: {end - start:.2f} seconds, Backtracking calls saved: {board.backtrack_calls-backtracks}")
+
+"""
+with Live(Fboard, refresh_per_second=20) as live:
+    start = time.time()
+    correct, backtracks = backtrackingWithForwardChecking(Fboard,Fboard.select_unassigned_cell(),0)
+    end = time.time()
+
+    print(f"Forward Checking: Time taken: {end - start:.2f} seconds, Backtracking calls saved: {board.backtrack_calls-backtracks}")
+"""
+
+
+
+#Gathering Times for benchmarking
+"""timeBacktrack = []
+timeBacktrackMod = []
+backtracksSaved = []
+n = 100
+difficulty = 3
+for i in range(n):
+    
+    
+    board = Board.load_random_puzzle(difficulty=difficulty)
+    boardmod = copy.deepcopy(board)
+
+    requiredBacktrack = board.backtrack_calls
+    print(f"\rpuzzle: {i+1} required backtracks {requiredBacktrack}\n","\033[A\033[A")
+    
+
+    start = time.time()
+    correct, backtracks = backtracking(board, 0,0,0) # normal backtracking
+    end = time.time()
+
+    timeBacktrack.append(end-start)
+
+    start = time.time()
+    correct, backtracks = backtrackModified(boardmod, boardmod.select_unassigned_cell(),0) # modified backtracking
+    end = time.time()
+
+    backtracksSaved.append(requiredBacktrack-backtracks)
+    timeBacktrackMod.append(end-start)
+
+
+
+print(f" For {n} killer sudoku problems of difficulty {difficulty}:")
+print(f" Average Time for Backtrack Modified: {sum(timeBacktrackMod)/n:.2f}\n",
+      f"Average Time for Normal Backtrack  : {sum(timeBacktrack)/n:.2f}\n",
+      f"Average Backtrack Saved            : {sum(backtracksSaved)/n}"
+      )
+"""
--- code/models/backtracking.py
+++ code/models/backtracking.py
@@ -1,9 +1,137 @@
-def backtracking(board, domain):
-    for i in range(9):
-        for j in range(9):
-            num = int(input())
-            print(i, j)
-            board.cells[i][j].value = num
-            print(board.is_valid(board.cells[i][j]))
-            print(f"Solved: {board.is_solved()}")
-            print(board)
+import copy
+import time
+
+
+from rich import cells
+from rich.live import Live
+from models.board import Board
+
+def backtracking(board,row, col,count):
+    count += 1
+
+    #check if we have reached the end of the board and if it is solved
+    if row == 8 and col == 9 and board.is_solved():
+        return True, count
+    
+    #move down a row once one is complete
+    if col == 9:
+        row += 1
+        col = 0
+
+    #retrieve the domain of the current cell
+    domain = board.cells[row][col].domains
+
+    # if the cell is already filled, move to the next one
+    if board.cells[row][col].value is not None:
+        return backtracking(board, row, col+1, count)
+    
+    #iterate through the domain of the current cell and try to assign a value
+    for value in domain:
+        board.cells[row][col].value = value
+        if board.is_valid(board.cells[row][col]):
+            #if value is valid, move to the next cell
+            
+            found, count = backtracking(board, row, col+1, count)
+
+            if found:
+                return True, count
+            
+        #if value is not valid, reset the cell and try the next value in the domain
+        board.cells[row][col].value = None
+
+    return False, count
+
+
+def backtrackingWithAC3(board,row, col,count):
+    count += 1
+    board.revise_arcs()
+
+    #check if we have reached the end of the board and if it is solved
+    if row == 8 and col == 9 and board.is_solved():
+        return True, count
+    
+    #move down a row once one is complete
+    if col == 9:
+        row += 1
+        col = 0
+
+    #retrieve the domain of the current cell
+    domain = board.cells[row][col].domains
+
+    # if the cell is already filled, move to the next one
+    if board.cells[row][col].value is not None:
+        return backtrackingWithAC3(board, row, col+1, count)
+    
+    #iterate through the domain of the current cell and try to assign a value
+    for value in domain:
+        board.cells[row][col].value = value
+        if board.is_valid(board.cells[row][col]):
+            #if value is valid, move to the next cell
+            
+            found, count = backtrackingWithAC3(board, row, col+1, count)
+
+            if found:
+                return True, count
+            
+        #if value is not valid, reset the cell and try the next value in the domain
+        board.cells[row][col].value = None
+
+    return False, count
+
+def backtrackModified(board,cell,count):
+     #This backtracking version has arc revision, MRV heuristic, and LCS heuristic
+    count +=1
+    board.revise_arcs()
+    #print(f"Backtracking call count: {count}")
+
+    #check if we have reached the end of the board and if it is solved
+    if board.is_solved():
+        return True, count
+    domain  = cell.domains.intersection(cell.cage.getDomain())
+    
+
+    if len(domain)==1: 
+        cell.value = list(domain)[0]
+        if board.is_valid(cell):
+            #if value is valid, move  to the next cell
+            #revise domains
+            found, count = backtrackModified(board,board.select_unassigned_cell(), count)
+
+            if found:
+                return True, count
+            
+    else:
+        found, count = backtracking(board,0,0, count)
+        if found:
+            return True, count
+
+    return False, count
+
+def backtrackingWithForwardChecking(board,cell,count):
+    #This backtracking version has arc revision, MRV heuristic, and LCS heuristic
+    count +=1
+    board.revise_arcs()
+    #print(count)
+    #print(f"Backtracking call count: {count}")
+
+    #check if we have reached the end of the board and if it is solved
+    if board.is_solved():
+        return True, count
+    lcsValues = board.findLCS(cell)
+    lcsValues.sort() # sort values by least constraining 
+    #iterate through the domain of the current cell and try to assign a value
+    for value in lcsValues:
+        cell.value = value[1]
+        if board.is_valid(cell):
+            #if value is valid, move  to the next cell
+            #revise domains
+            found, count = backtrackingWithForwardChecking(board,board.select_unassigned_cell(), count)
+
+            if found:
+                return True, count
+            
+        cell.value = None
+
+    return False, count
+
+
